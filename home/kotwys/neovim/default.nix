9f2aa6ce8d28f865c807d944e44c16fba4f396ce
kotwys <52920928+kotwys@users.noreply.github.com>
2021-07-13 13:21:13 +0400

    neovim: Add Goyo.vim

--- home/kotwys/neovim/default.nix
+++ home/kotwys/neovim/default.nix
@@ -24,6 +24,7 @@
         "LnL7/vim-nix"
         "vim-pandoc/vim-pandoc"
         "vim-pandoc/vim-pandoc-syntax"
+        "junegunn/goyo.vim"
       ];
       after = builtins.readFile ./plugins.vim;
     };
