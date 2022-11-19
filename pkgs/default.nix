ab1d6400499f0f44bc1d3b2d06c6ffa17d36a24a
kotwys <52920928+kotwys@users.noreply.github.com>
2022-11-19 23:37:21 +0400

    pkgs: Remove pascal-language-server

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,5 +4,4 @@
   epson_201401w = pkgs.callPackage ./misc/drivers/epson_201401w { };
   tl-minecraft = pkgs.callPackage ./games/tl-minecraft { };
   memento = pkgs.libsForQt5.callPackage ./applications/video/memento.nix { };
-  pascal-language-server = pkgs.callPackage ./development/tools/pascal-language-server { };
 }
