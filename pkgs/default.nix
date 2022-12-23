e6efe75102ab43a16c0fc4bbb0a889622f9798cd
kotwys <52920928+kotwys@users.noreply.github.com>
2022-12-23 21:17:53 +0400

    pkgs: Remove tl-minecraft
    Removed the faulty packaging due to the addition of ch.tlaun.TL to
    the Flathub repository.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,7 +2,6 @@
 
 {
   epson_201401w = pkgs.callPackage ./misc/drivers/epson_201401w { };
-  tl-minecraft = pkgs.callPackage ./games/tl-minecraft { };
   memento = pkgs.libsForQt5.callPackage ./applications/video/memento.nix { };
   pascalabcnet = pkgs.callPackage ./development/compilers/pascalabcnet { };
 }
