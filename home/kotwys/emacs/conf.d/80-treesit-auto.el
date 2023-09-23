939cbc0b50760ed6ebc5e2e9e3dac79185b18133
kotwys <52920928+kotwys@users.noreply.github.com>
2023-09-23 15:24:24 +0400

    home: Exclude HTML from treesit-auto

--- home/kotwys/emacs/conf.d/80-treesit-auto.el
+++ home/kotwys/emacs/conf.d/80-treesit-auto.el
@@ -1,4 +1,8 @@
 (require 'treesit-auto)
 (global-treesit-auto-mode)
 
+(setq treesit-auto-recipe-list
+      (seq-filter (lambda (r)
+                    (not (eq (treesit-auto-recipe-lang r) 'html)))
+                  treesit-auto-recipe-list))
 (add-to-list 'auto-mode-alist '("\\.ya?ml\\'" . yaml-ts-mode))
