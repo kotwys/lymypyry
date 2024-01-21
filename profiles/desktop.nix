f65b720fb80884054611ff0bba49c02b41acdaf8
kotwys <52920928+kotwys@users.noreply.github.com>
2024-01-21 15:21:14 +0400

    profiles/desktop: Add Pinyin input

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -11,7 +11,7 @@
     useXkbConfig = true;
   };
   i18n.inputMethod.enabled = "ibus";
-  i18n.inputMethod.ibus.engines = with pkgs.ibus-engines; [ mozc ];
+  i18n.inputMethod.ibus.engines = with pkgs.ibus-engines; [ mozc libpinyin ];
 
   services.xserver = {
     enable = true;
