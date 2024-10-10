e4e457523ef0532a56eff74b2dd65d4a04e53d49
Kočyš Mikajlo <52920928+kotwys@users.noreply.github.com>
2024-10-10 17:04:49 +0400

    home/zsh: Autocompletion and suggestions

--- home/kotwys/zsh/default.nix
+++ home/kotwys/zsh/default.nix
@@ -1,11 +1,23 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.zsh = {
     enable = true;
     defaultKeymap = "emacs";
-    initExtra = builtins.readFile ./init.zsh;
+    initExtra = ''
+      setopt auto_pushd
+      setopt pushd_ignore_dups
+    '';
+    completionInit = ''
+      zstyle ':completion:*' matcher-list 'm:{a-zA-Zа-яёА-ЯЁ}={A-Za-zА-ЯЁа-яё}'
+      zstyle ':completion:*' menu select
+    '';
     history.ignoreAllDups = true;
+    historySubstringSearch = {
+      enable = true;
+      searchUpKey = "$terminfo[kcuu1]";
+      searchDownKey = "$terminfo[kcud1]";
+    };
     syntaxHighlighting = {
       enable = true;
       styles = {
@@ -13,5 +25,12 @@
         double-hyphen-option = "fg=blue";
       };
     };
+    plugins = [
+      {
+        name = "zsh-autosuggestions";
+        src = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
+        file = "zsh-autosuggestions.zsh";
+      }
+    ];
   };
 }
