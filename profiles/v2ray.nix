2dcea37223480a53ea08d7cfb241be998b02ab1d
Kočyš Mikajlo <52920928+kotwys@users.noreply.github.com>
2025-01-03 20:49:32 +0400

    home: Use v2rayA

--- profiles/v2ray.nix
+++ profiles/v2ray.nix
@@ -1,13 +1,26 @@
 { pkgs, ... }:
 
-let
-  inherit (pkgs) v2ray;
-in
 {
-  systemd.packages = [ v2ray ];
+  environment.systemPackages = [ pkgs.v2raya ];
 
-  systemd.services.v2ray = {
-    # https://github.com/NixOS/nixpkgs/issues/81138
+  systemd.services.v2raya = {
+    description = "v2rayA";
+    after = [ "network.target" "iptables.service" ];
+    wants = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
+    path = builtins.attrValues {
+      inherit (pkgs) iptables bash;
+    };
+
+    /* https://github.com/NixOS/nixpkgs/blob/nixos-24.05/nixos/modules/services/networking/v2raya.nix */
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.v2raya}/bin/v2rayA --log-disable-timestamp";
+      Environment = [ "V2RAYA_LOG_FILE=/var/log/v2raya/v2raya.log" ];
+      User = "root";
+      LimitNPROC = 500;
+      LimitNOFILE = 1000000;
+      Restart = "on-failure";
+    };
   };
 }
