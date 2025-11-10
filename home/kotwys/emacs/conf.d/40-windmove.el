7499ec057258caf7e728ac74b62f8c89ee5f3cac
Kočyš Mikajlo <52920928+kotwys@users.noreply.github.com>
2025-11-11 00:01:52 +0400

    home/emacs: Window moving keys

---
+++ home/kotwys/emacs/conf.d/40-windmove.el
@@ -0,0 +1,6 @@
+;; -*- lexical-binding: t -*-
+
+(global-set-key (kbd "C-x w h") 'windmove-left)
+(global-set-key (kbd "C-x w j") 'windmove-down)
+(global-set-key (kbd "C-x w k") 'windmove-up)
+(global-set-key (kbd "C-x w l") 'windmove-right)
